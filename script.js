43be367d5a0b4d49a154b4165f2ec4812e7d3be2
JulianaAparicio <juliana_aparicio@outlook.com>
2022-04-16 15:03:05 -0300

    Otra forma de hacerlo

--- script.js
+++ script.js
@@ -23,60 +23,25 @@ async function fillPokemonData(name, order) {
 
     //ESCRIBE TU CÓDIGO A CONTINUACIÓN DENTRO DE LA FUNCIÓN:
 
+    // Ver que contiene
     //console.log(pokemonData.stats);
 
-    agregarColorBarra("hp", order, obtenerStats(pokemonData, "hp"));
-    agregarColorBarra("ataque", order, obtenerStats(pokemonData, "ataque"));
-    agregarColorBarra("defensa", order, obtenerStats(pokemonData, "defensa"));
-    agregarColorBarra("velocidad", order, obtenerStats(pokemonData, "velocidad"));
-}
-
-// Retorna la cantidad del stats según el nombre del mismo:
-function obtenerStats(data, tipo) {
-    return data.stats.find(element => {
-        if (element.name === tipo)
-            return element;
-    }).amount;
-}
-
-// Esta funcion recibe el nombre del stat, el orden del pokemon y el puntaje correspondiente:
-function agregarColorBarra(tipoStat, order, puntos) {
     // Seleccionamos la barra
-    const barra = document.querySelector(`#barra-${tipoStat}-${order}`);
-    // Inicializamos el progreso en 0, para asegurarnos
-    // que siempre arranque desde el principio
-    let width = 0;
-    const tiempoCarga = 2000;
-    // Calculamos el progreso en base al tiempo total de carga
-    const progresoCarga = tiempoCarga / 100;
-    let intervalo = setInterval(aumentarProgreso, progresoCarga);
+    pokemonData.stats.forEach((stat) => {
+        const barra = document.querySelector(`#barra-${stat.name}-${order}`);
 
-    function aumentarProgreso() {
-        // Si el progreso esta completo, detenemos el programa:
-        if (width >= puntos) {
-            clearInterval(intervalo);
-            // Sino le agregamos la clase que corresponda:
+        // se rellena según el valor del stat
+        barra.style.width = `${stat.amount}%`;
+
+        if (stat.amount < 35) {
+            barra.classList.add("rojo");
+        } else if (stat.amount < 70) {
+            barra.classList.add("amarillo");
         } else {
-            if (width < 35) {
-                barra.classList.add("rojo");
-                barra.classList.remove("amarillo");
-                barra.classList.remove("verde");
-            } else if (width <= 70) {
-                barra.classList.add("amarillo");
-                barra.classList.remove("rojo");
-                barra.classList.remove("verde");
-            } else {
-                barra.classList.add("verde");
-                barra.classList.remove("amarillo");
-                barra.classList.remove("rojo");
-            }
-            width++;
+            barra.classList.add("verde");
         }
-        // Modificamos el DOM, para impactar el nuevo progreso:
-        barra.style.width = width + "%";
-    }
+    });
 }
-
 
 
 //LISTADO DE POKEMONS - PUEDES CAMBIAR POR TU FAVORITO!
